40a1d86e7c81b12407ede390647e91a8ac68c245
me <cocomelonjuice293@gmail.com>
2024-10-16 00:39:26 +0700

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -95,7 +95,7 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Read our docs
+            Read our docs haha
           </a>
         </div>
       </main>
